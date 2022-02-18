6e64c0583491a89327b583e2359cef81f52d95b8
ah-nemati <amirhoseinnemati79@gmail.com>
2022-02-18 10:26:24 +0330

    1.1.0

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { About } from "./pages/About";
 import { User } from "./pages/User";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="user" element={<User />} />
       </Routes>
-    </BrowserRouter>
   );
 }
 
